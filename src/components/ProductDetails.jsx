6718b4f452b163545c965bb13a49899c5530da8c
VenkateshMogili <mogilivenkatesh3@gmail.com>
2024-11-22 21:42:33 +0530

    Product Details added

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,5 +1,14 @@
+import { useParams } from "react-router-dom"
+import Product from "./Product";
+import { products } from "../utils/products";
+
 export default function ProductDetails() {
+  const {id}=useParams();
+
+  const product=products.find((product)=>product.id==id);
   return (
-    <div>ProductDetails</div>
+    <div>
+      <Product product={product} size={12} moreInfo={true}/>
+    </div>
   )
 }
